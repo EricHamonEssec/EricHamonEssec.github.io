59025809f14fb91e0dc8fa82b2e484d43519d15a
Eric HAMON <eric.hamon@essec.edu>
2025-06-02 21:09:40 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/EricHamonEssec.github.io/', // Match your repository name
+  base: '/', // Match your repository name
   build: {
     outDir: 'dist',
   },
